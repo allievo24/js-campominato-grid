9c7d61799565f67de8be647edf8026a32799bc88
saidmoussadek <saiddus24@gmail.com>
2022-07-14 15:48:40 +0200

    push

--- js/main.js
+++ js/main.js
@@ -1,19 +1,42 @@
 const grigliaDom= document.getElementById('griglia');
 
-for( i=0; i<100; i++){
- const quadratoGrglia = document.createElement('div');
- quadratoGrglia .classList.add ('quadrato');
- quadratoGrglia .addEventListener('click',
-       function(){
-             this.classList.toggle('bg-color')
+for( i=1; i<=100; i++){
+ creaGriglia();
+}
 
 
-       }
- 
- 
- );
 
- 
- quadratoGrglia.append([i]);
- grigliaDom.append(quadratoGrglia);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function creaGriglia(){
+
+    const quadratoGrglia = document.createElement('div');
+    quadratoGrglia .classList.add ('quadrato');
+    quadratoGrglia .addEventListener('click',
+          function(){
+                this.classList.toggle('bg-color')
+                console.log(quadratoGrglia);
+   
+          }
+    );
+   
+    quadratoGrglia.append([i]);
+    grigliaDom.append(quadratoGrglia); 
 }
